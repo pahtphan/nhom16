3146c4aaf90a89e5cd2638e56536be0cc4bb0aab
phanthanhphat5797@gmail.com <phanthanhphat5797@gmail.com>
2024-03-28 15:27:27 +0700

    update

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -6,7 +6,7 @@ class ContactController {
       res.render("contact/form", {});
     } catch (error) {
       // 500 là lỗi internal server(Lỗi xảy ra ở server)
-      // console.log(error);Dành cho dev xem
+      console.log(error); //Dành cho dev xem
       res.status(500).send(error.message); //Cho người dùng xem
     }
   };
@@ -35,7 +35,7 @@ class ContactController {
 
     } catch (error) {
       // 500 là lỗi internal server(Lỗi xảy ra ở server)
-      // console.log(error);Dành cho dev xem
+      console.log(error); //Dành cho dev xem
       res.status(500).send(error.message); //Cho người dùng xem
     }
   };
